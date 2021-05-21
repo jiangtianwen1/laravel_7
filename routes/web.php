72ddf845ba56fddcf1486b8505222e6e3eb59104
mr.jiang <1062882647@qq.com>
2021-05-21 07:23:51 +0000

    用户注册

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController@about');
+
+
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+route::get('signup','UsersController@create')->name('signup');
 
